5e997e36c83d3b8a17ebf00c3d2fbfa0eeae3cdf
syunnjack <syunnda1@yahoo.co.jp>
2026-08-05 20:25:43 +0900

    Add Google Search Console verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,8 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang="ja"><head><script async src="https://www.googletagmanager.com/gtag/js?id=G-71LHXQP189"></script><script dangerouslySetInnerHTML={{__html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-71LHXQP189');`}} /></head>
+        <html lang="ja"><head>
+  <meta name="google-site-verification" content="3ong__EaMNOGlOKlUdLEOYCRXWEkEwOVq0wwNnPf0YM" /><script async src="https://www.googletagmanager.com/gtag/js?id=G-71LHXQP189"></script><script dangerouslySetInnerHTML={{__html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-71LHXQP189');`}} /></head>
             <body>{children}</body>
         </html>
     );
